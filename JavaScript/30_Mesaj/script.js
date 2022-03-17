a533e02feef3e58501694a0197c89a424ab97be9
Muratkscnn <muratkscnn@gmail.com>
2022-03-17 14:58:11 +0300

    17.03.2022

--- JavaScript/30_Mesaj/script.js
+++ JavaScript/30_Mesaj/script.js
@@ -1,6 +1,22 @@
-const form=document.querySelector("form");
+const form = document.querySelector("form");
+const label = document.querySelector(".label")
 form.addEventListener("submit",e=>{
     e.preventDefault();
-    alert(form.userName.value);
+    const userName=form.userName.value;
+    const userNameModel=/^[a-z]{6,10}$/; //regexkodları
+    if (userNameModel.test(userName)) {
+        label.innerHTML="<label>Kullanıcı Adı Kullanılabilir</label>"
+        form.userName.classList.add("succes");
+        form.userName.classList.remove("error");
 
-})
+    }
+    else{
+        label.innerHTML="<label>Hatalı Giriş</label>";
+        form.userName.classList.add("error");
+        form.userName.classList.remove("succes");
+        
+    }
+
+    
+
+});
