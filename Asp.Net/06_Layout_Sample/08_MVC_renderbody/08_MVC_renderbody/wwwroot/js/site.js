e6a319ee309a8916e5f9f708e043074a86f96db3
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-04-06 14:56:03 +0300

    6.04.2022

--- Asp.Net/06_Layout_Sample/08_MVC_renderbody/08_MVC_renderbody/wwwroot/js/site.js
+++ Asp.Net/06_Layout_Sample/08_MVC_renderbody/08_MVC_renderbody/wwwroot/js/site.js
@@ -1,9 +1,11 @@
 ﻿$(document).ready(function () {
     $("li a").each(function () {
+        console.log($(this))
         if ($(this).attr("href") == window.location.pathname)
             
             $(this).addClass("active");
     })
+   
 })
 
 
