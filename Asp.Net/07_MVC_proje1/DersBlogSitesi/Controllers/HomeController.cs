774c45bea9f57ba6a481c7628fa496e628c79d81
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-04-07 14:46:36 +0300

    7.04.22

--- Asp.Net/07_MVC_proje1/DersBlogSitesi/Controllers/HomeController.cs
+++ Asp.Net/07_MVC_proje1/DersBlogSitesi/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DersBlogSitesi.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,27 @@ namespace DersBlogSitesi.Controllers
 {
     public class HomeController : Controller
     {
+        List<Article> makaleler = new List<Article>
+            {
+            new Article {ID=1, Header="HTML", Content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!"},
+            new Article { ID = 2, Header = "CSS", Content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!" },
+            new Article { ID = 3, Header = "Bootstrap", Content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!" },
+            new Article { ID = 4, Header = "JavaScript", Content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!" },
+            new Article { ID = 5, Header = "C#", Content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!" },
+            new Article { ID = 6, Header = "JQuery", Content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus eveniet fugiat voluptate natus quia dolor modi corporis cumque ad, nostrum, vel cupiditate sapiente atque quibusdam pariatur magni molestias accusantium deleniti omnis harum ducimus iste placeat aperiam commodi! Eveniet, ipsam beatae quas, rerum repudiandae inventore quae, est reprehenderit laboriosam sequi sit!" }
+
+            };
+
         public IActionResult Index()
         {
-            return View();
+            
+
+            return View(makaleler);
         }
+        public IActionResult HTML()
+        {
+            return View(makaleler);
+        }
+       
     }
 }
---
+++ Asp.Net/07_MVC_proje1/DersBlogSitesi/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DersBlogSitesi.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Header { get; set; }
+        public string Content { get; set; }
+    }
+}
