16a545d2297d84d98db90f56ec9d199b702b476c
Muratkscnn <98876141+Muratkscnn@users.noreply.github.com>
2022-04-12 14:51:58 +0300

    12.04.2022

---
+++ Asp.Net/Dikencomtr_clone/Dikencomtr_clone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dikencomtr_clone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
